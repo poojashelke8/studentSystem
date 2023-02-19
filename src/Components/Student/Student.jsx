460bda5576ae30a2c6f4220494bbda33e3fcd564
pshelke <pshelke@taranawireless.com>
2023-02-19 16:41:55 +0530

    removed unnecessary dependencies

--- src/Components/Student/Student.jsx
+++ src/Components/Student/Student.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AddStudent from './AddStudent'
-import { useState,useEffect } from 'react'
+import { useState} from 'react'
 import StudentTable from './StudentTable';
 
 
